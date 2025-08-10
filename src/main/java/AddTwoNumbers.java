49a6a8498f499e17142332d97e193d3186f67a0f
NadavNV <nadav.nevo@gmail.com>
2025-08-10 10:40:12 +0300

    Working on problems

---
+++ src/main/java/AddTwoNumbers.java
@@ -0,0 +1,38 @@
+public class AddTwoNumbers {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode start = new ListNode();
+        ListNode previous = start;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int digit = x + y + carry;
+            carry = 0;
+            if (digit >= 10) {
+                carry = digit / 10;
+                digit = digit % 10;
+            }
+            previous.next = new ListNode(digit);
+            previous = previous.next;
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+        return start.next;
+    }
+}
--- src/main/java/ReorderedPowerOfTwo.java
+++ src/main/java/ReorderedPowerOfTwo.java
@@ -1,53 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class ReorderedPowerOfTwo {
-    public static class Permute {
-        // https://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order
-        private int[] indexes;
-        private String source;
+    private static final Map<String, Integer> powersOfTwo = new HashMap<>();
 
-        public Permute(String source) {
-            this.source = source;
-            indexes = new int[source.length()];
-            for (int i = 0; i < indexes.length; i++) {
-                indexes[i] = i;
+    static {
+        String digits;
+        for (int i = 0; Math.pow(2, i) < Math.pow(10, 9); i++) {
+            digits = countDigits((int) Math.pow(2, i));
+            if (powersOfTwo.containsKey(digits)) {
+                powersOfTwo.put(digits, powersOfTwo.get(digits) + 1);
+            } else {
+                powersOfTwo.put(digits, 1);
             }
-        }
-
-        public String getNext() {
-            StringBuilder stringBuilder = new StringBuilder();
-            for (int index : indexes) {
-                stringBuilder.append(source.charAt(index));
-            }
-            return stringBuilder.toString();
-        }
-
-        public boolean hasNext() {
-            int i, j, k;
-            // Find maximum index such that indexes[j+1]>indexes[j]
-            for (j = indexes.length - 2; j >= 0; j--) {
-                if (indexes[j + 1] > indexes[j]) {
-                    break;
-                }
-            }
-            if (j == -1) {
-                return false;
-            }
-            // Find maximum index such that indexes[k] > indexes[j]
-            for (k = indexes.length - 1; k > j; k--) {
-                if (indexes[k] > indexes[j]) {
-                    break;
-                }
-            }
-            // Swap indexes[k], indexes[j]
-            int temp = indexes[j];
-            indexes[j] = indexes[k];
-            indexes[k] = temp;
-            // Reverse indexes from j+1
-            for (i = j + 1; 2 * i <= indexes.length + j; i++) {
-                temp = indexes[i];
-                indexes[i] = indexes[indexes.length - i + j];
-                indexes[indexes.length - i + j] = temp;
-            }
-            return true;
         }
     }
 
@@ -55,20 +20,12 @@ public class ReorderedPowerOfTwo {
         if (Integer.bitCount(n) == 1) {
             return true;
         }
-        Permute permute = new Permute(String.valueOf(n));
-        while (permute.hasNext()) {
-            String number = permute.getNext();
-            if (!number.startsWith("0")) {
-                if (Integer.bitCount(Integer.parseInt(number)) == 1) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return powersOfTwo.containsKey(countDigits(n));
     }
 
-    private String countDigits(int n) {
+    private static String countDigits(int n) {
         int[] histogram = new int[10];
+        int original = n;
         while (n > 0) {
             histogram[n % 10]++;
             n = Math.floorDiv(n, 10);
@@ -79,6 +36,4 @@ public class ReorderedPowerOfTwo {
         }
         return sb.toString();
     }
-
-
 }
